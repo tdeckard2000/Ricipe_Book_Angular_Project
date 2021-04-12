a76b394209a9f54f42ec8350fd05a1fb69880380
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2021-04-11 20:43:18 -0600

    Create App Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './shoppingList/list/list.component';
+import { ListEditComponent } from './shoppingList/list-edit/list-edit.component';
+import { IngredientsComponent } from './shoppingList/ingredients/ingredients.component';
+import {RecipeComponent } from './recipeBook/recipe/recipe.component';
+import {RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import {RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
+import {RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    ListEditComponent,
+    IngredientsComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
